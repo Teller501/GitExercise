71001bcfbe110294266f3566a86950568f6c3745
Teller501 <ande777j@stud.kea.dk>
2022-09-09 13:23:34 +0200

    initial commit

---
+++ src/SuperGreeter.java
@@ -0,0 +1,12 @@
+public class SuperGreeter {
+
+    public static void main(String[] args) {
+        SuperGreeter superGreeter = new SuperGreeter();
+
+        superGreeter.start();
+    }
+
+    public void start(){
+        System.out.println("Det virker!");
+    }
+}
