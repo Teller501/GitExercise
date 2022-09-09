d1d3401cadf2139166dc438e4780fcda325c0188
Teller501 <ande777j@stud.kea.dk>
2022-09-09 13:25:29 +0200

    added greet

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,12 +1,24 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
+
+    Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         SuperGreeter superGreeter = new SuperGreeter();
 
         superGreeter.start();
+        superGreeter.Greet();
     }
 
     public void start(){
         System.out.println("Det virker!");
+    }
+
+    public void Greet(){
+        System.out.println("Indtast dit navn");
+        String navn = scanner.next();
+
+        System.out.println("Hej " + navn);
     }
 }
