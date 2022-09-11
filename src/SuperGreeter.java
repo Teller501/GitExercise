65fc4c5b1d8980e45dfb14e63985a8b4bbe3a25e
Teller501 <ande777j@stud.kea.dk>
2022-09-11 18:26:23 +0200

    added askAboutMood method

--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -4,6 +4,7 @@ public class SuperGreeter {
 
     Scanner scanner = new Scanner(System.in);
     private int age;
+    private String navn;
 
     public static void main(String[] args) {
         SuperGreeter superGreeter = new SuperGreeter();
@@ -14,18 +15,40 @@ public class SuperGreeter {
     }
 
     public void start(){
+
+        // Får alder som input fra bruger
         System.out.print("Hvor gammel er du? ");
         age = scanner.nextInt();
-        System.out.println("Du er " + age + " år gammel");
-        System.out.println("Du er født i " + calculateYear() + " eller i " + (calculateYear() - 1));
+        System.out.println("Du er " + age + " år gammel"); // Printer brugers alder
+        System.out.println("Du er født i " + calculateYear() + " eller i " + (calculateYear() - 1)); // Printer årstal bruger er født i
+
+        askAboutMood();
+    }
+
+    private void askAboutMood() {
+        char answer;
+        do {
+            System.out.print("Har du det godt, " + navn + "? (j/n) ");
+            answer = scanner.next().charAt(0);
+
+            if (answer == 'j') {
+                System.out.println("Det er godt at høre!");
+            } else if (answer == 'n') {
+                System.out.println("Det var knap så godt, drik en bajer!");
+            } else {
+                System.out.println("Ugyldigt input");
+            }
+
+        }while (answer != 'j' && answer != 'n') ;
     }
 
     public void Greet(){
         System.out.print("Indtast dit navn: ");
-        String navn = scanner.next();
+        navn = scanner.next();
 
         System.out.println("Hej " + navn);
     }
+
 
 
     public int calculateYear(){
